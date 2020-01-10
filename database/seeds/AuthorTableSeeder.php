49906b3f72b59fa250ab287da378e4abc00698d6
Andy Nur <andylulz@yahoo.com>
2020-01-10 10:28:02 +0800

    author seeder & model

---
+++ database/seeds/AuthorTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Author;
+
+class AuthorTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = array();
+        foreach (range(1, 10) as $index) {
+            $data[] = [
+                'display_name' => 'Author '. $index,
+                'first_name' => 'author',
+                'last_name' => 'postingan '. $index,
+            ];
+        }
+
+        Author::insert($data);
+    }
+}
